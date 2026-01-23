77648b3e2a6ebf4f485e8bd22beca9e7dbd5b3f3
anna belenko <66851299+annabelenko@users.noreply.github.com>
2026-01-23 11:31:47 -0500

    add about section

--- src/App.js
+++ src/App.js
@@ -4,14 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <object 
-        data="/images/hero.svg" 
-        type="image/svg+xml"
-        className="hero-svg"
-        aria-label="Hack Brooklyn"
-      >
-        <img src="/images/hero.svg" alt="Hack Brooklyn" className="hero-image" />
-      </object>
+      <div className="section">
+        <object 
+          data="/images/hero.svg" 
+          type="image/svg+xml"
+          className="hero-svg"
+          aria-label="Hack Brooklyn"
+        >
+          <img src="/images/hero.svg" alt="Hack Brooklyn" className="hero-image" />
+        </object>
+      </div>
+      <div className="section">
+        <object 
+          data="/images/about.svg" 
+          type="image/svg+xml"
+          className="hero-svg"
+          aria-label="About"
+        >
+          <img src="/images/about.svg" alt="About" className="hero-image" />
+        </object>
+      </div>
     </div>
   );
 }
