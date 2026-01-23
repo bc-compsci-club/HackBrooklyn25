b76391898cadfa6c75999bf67d536abec3004339
anna belenko <66851299+annabelenko@users.noreply.github.com>
2026-01-23 12:15:38 -0500

    added sponsor button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img src="/images/heroo.svg" alt="Hero" />
+      <div className="hero-section">
+        <img src="/images/heroo.svg" alt="Hero" />
+        <a href="mailto:bc.cis.club@gmail.com" className="sponsor-button">Sponsor Us</a>
+        <p className="sponsor-email">Or email bc.cis.club@gmail.com</p>
+      </div>
       <img src="/images/about.svg" alt="About" />
     </div>
   );
