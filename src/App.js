c54240e9c0bcadc4b2545df1f7660aec8d75a6c6
birongliu <62890027+birongliu@users.noreply.github.com>
2026-03-17 09:01:01 -0400

    Add shelf SVG image and comment out Tracks component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react"
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Footer from "./components/Footer";
-import Tracks from "./components/Tracks";
+// import Tracks from "./components/Tracks";
 
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
-      <Tracks/>
+      {/* <Tracks/> */}
       <Footer/>
     </div>
   );
