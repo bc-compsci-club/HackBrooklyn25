08c6cfc2bc50e5c375ee4170029bb9a2db8d2578
anna belenko <66851299+annabelenko@users.noreply.github.com>
2026-03-29 11:44:21 -0400

    added mentors and prizes information

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Tracks from "./components/Tracks";
+import Speakers from "./components/Speakers";
+import Prizes from "./components/Prizes";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Hero/>
       <About/>
       <Tracks/>
+      <Speakers/>
+      <Prizes/>
       <Footer/>
     </div>
   );
